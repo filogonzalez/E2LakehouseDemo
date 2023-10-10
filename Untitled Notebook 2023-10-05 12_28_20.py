6652def5549bbd8d16f17d67ae62af8640ba0f4a
filobricks <filobricks>
2023-10-10 02:42:01 +0000

    commit to MAIN

---
+++ Untitled Notebook 2023-10-05 12_28_20.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+# MAGIC %scala
+
+# COMMAND ----------
+
+# MAGIC %r
+
+# COMMAND ----------
+
+# MAGIC %sql
+
+# COMMAND ----------
+
+print('Hi')
+
+# COMMAND ----------
+
+
